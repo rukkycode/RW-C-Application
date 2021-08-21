4208b94c2935a5416ef04b63d4e1cb30d166ebf1
rukkycode <74117698+rukkycode@users.noreply.github.com>
2021-08-21 13:05:59 -0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RW_C_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Strings
+            string appname = "RW C# Application";
+            string version = "1.0.0";
+
+            // App Startup
+            Console.WriteLine("Welcome to "+appname+" Version: "+version);
+
+            Console.WriteLine(" _______");
+            Console.WriteLine("|       |");
+            Console.WriteLine("|       |");
+            Console.WriteLine("|_______|");
+
+            Console.WriteLine();
+            Console.WriteLine("Whats your Player Name?");
+
+            string playerName = Console.ReadLine();
+
+            Console.WriteLine("Welcome to Rukky's World "+playerName+"!");
+        }
+    }
+}
